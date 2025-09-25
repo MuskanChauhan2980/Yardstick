251a8c5f6cc210d3c2e7ff4d04ab9b45ce0987d0
Muskan Chauhan <muskan.chauhan@redknot.casa>
2025-09-25 20:30:03 +0530

    fix the error in the frontend deployment

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/loginPage';
 import NotesPage from './pages/NotesPage';
 import axios from 'axios';
 
